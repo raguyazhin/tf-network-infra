b31621b0ae1228d79a37fe43e600a7ec7920cd0f
Ragu Thangavel <raguyazhin@gmail.com>
2024-01-26 16:41:52 +0000

    modified

--- env/dev.tfvars
+++
@@ -1,4 +0,0 @@
-region     = "us-east-1"
-vpc_cidr   = "10.0.0.0/16"
-vpc_name   = "dev-vpc"
-environment = "dev"
--- variables.tf
+++ variables.tf
@@ -1,19 +1,17 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
+  default = "10.1.0.0/16"
 }
 
 variable "vpc_name" {
   description = "Name for VPC"
   type        = string
-}
-
-variable "environment" {
-  description = "Environment (e.g., dev, prod)"
-  type        = string
+  default = "prod-vpc"
 }
