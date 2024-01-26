f1a745ec9bbd08b2b72b039fb6323667541823a4
Ragu Thangavel <raguyazhin@gmail.com>
2024-01-26 18:09:53 +0000

    modified

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_route_table" "private_route_table" {
 }
 
 resource "aws_route" "private_route" {
-  count          = length(var.private_subnet_cidr_blocks)
+  //count          = length(var.private_subnet_cidr_blocks)
   route_table_id = aws_route_table.private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.my_nat_gateway.id
